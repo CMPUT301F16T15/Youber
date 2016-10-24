db791a99d453628ec1ac22ce3f91ad7b4ffad465
rmaarouf <rmaarouf@ualberta.ca>
2016-10-24 13:37:27 -0600

    RiderTest class & StatusTest class are updated

---
+++ app/src/main/java/com/youber/cmput301f16t15/youber/FileManager.java
@@ -0,0 +1,12 @@
+package com.youber.cmput301f16t15.youber;
+
+/**
+ * Created by Reem on 2016-10-24.
+ */
+public class FileManager {
+
+    public static boolean loadFromfile() {
+        return false;
+    }
+
+}
--- app/src/test/java/com/youber/cmput301f16t15/youber/RiderTest.java
+++ app/src/test/java/com/youber/cmput301f16t15/youber/RiderTest.java
@@ -17,7 +17,7 @@ public class RiderTest
         Request request1 = new Request(geoLocation1, geoLocation2);
 
         Rider rider1 = new Rider();
-        rider1 = request1.addRider(rider1);
+        RequestController.addRequest(request1, rider1);
 
         rider1.makePayment(request1.getUUID());
         // actual cost will be 9.87 ($2 per km and $8 base fee)
@@ -34,6 +34,30 @@ public class RiderTest
         Rider currentUser = Helper.getCurrentRider();
         RequestCollection driverRequests = savedLocal.getRequestsForRiders(currentUser);
     }
+
+    // RequestController stores request in file when addRequest is called.
+    // US 08.02.01
+    public void testGetRequestsMadeOffline()
+    {
+        GeoLocation geoLocation1 = new GeoLocation(53.623236, -113.569712);
+        GeoLocation geoLocation2 = new GeoLocation(53.614820, -113.569697);
+
+        Request request1 = new Request(geoLocation1, geoLocation2);
+        Rider rider1 = new Rider();
+        RequestController.addRequest(request1, rider1);
+
+        assertTrue(FileManager.loadFromfile());
+
+    }
+
+
+    // US 08.03.01
+    public void testGetSentRequestsFileOffile()
+    {
+        assertTrue(FileManager.loadFromfile());
+    }
+
+
 
 
 }
