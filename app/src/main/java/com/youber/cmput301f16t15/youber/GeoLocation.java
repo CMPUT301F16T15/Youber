b6c98199053d6620cc0480ee8e6466d023980c33
aaronphilips <aphilips@ualberta.ca>
2016-10-27 16:05:18 -0600

    one more test works boissss

--- app/src/main/java/com/youber/cmput301f16t15/youber/GeoLocation.java
+++ app/src/main/java/com/youber/cmput301f16t15/youber/GeoLocation.java
@@ -1,13 +1,41 @@
 package com.youber.cmput301f16t15.youber;
 
+import java.util.Objects;
+
 /**
  * Created by Reem on 2016-10-13.
  */
 public class GeoLocation {
+    private double lat;
+    private double lon;
 
 
     public GeoLocation(double lat, double lon)
     {
+        this.lat=lat;
+        this.lon=lon;
+    }
 
+    public double getLat() {
+        return lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(o==null){
+            return false;
+        }
+        if(!GeoLocation.class.isAssignableFrom(o.getClass())){
+            return false;
+        }
+        final GeoLocation otherGeoLocation= (GeoLocation) o;
+        if(Math.abs(otherGeoLocation.getLat()-this.lat)<0.00001&&Math.abs(otherGeoLocation.getLon()-this.lon)<0.00001){
+            return true;
+        }
+        return false;
     }
 }
