ccaa2129d71d7a2c67b34a2ee423a2e2d6a25710
rmaarouf <rmaarouf@ualberta.ca>
2016-11-08 16:31:14 -0700

    Login Activity happening

--- app/src/main/java/com/youber/cmput301f16t15/youber/LoginActivity.java
+++ app/src/main/java/com/youber/cmput301f16t15/youber/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.youber.cmput301f16t15.youber;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,7 +12,28 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button loginButton = (Button) findViewById(R.id.loginbutton);
+        Button signUpButton = (Button) findViewById(R.id.signupbutton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+
+           }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
 
 
 }
