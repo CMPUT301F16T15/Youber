17209e595b27c789ab93b9d40f3a26867144b677
twmathie <twmathie@ualberta.ca>
2016-11-20 14:45:03 -0700

    required fields checks work and username is uneditable in profile -- leak when accepting requests

--- app/src/main/java/com/youber/cmput301f16t15/youber/gui/SignUpActivity.java
+++ app/src/main/java/com/youber/cmput301f16t15/youber/gui/SignUpActivity.java
@@ -1,12 +1,15 @@
 package com.youber.cmput301f16t15.youber.gui;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.youber.cmput301f16t15.youber.R;
 import com.youber.cmput301f16t15.youber.commands.AddUserCommand;
@@ -38,6 +41,10 @@ public class SignUpActivity extends AppCompatActivity implements NoticeDialogFra
     EditText firstName;
     EditText lastName;
 
+    TextView userString;
+    TextView phoneNumString;
+    TextView emailString;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,6 +55,11 @@ public class SignUpActivity extends AppCompatActivity implements NoticeDialogFra
         dateOfBirth = (EditText) findViewById(R.id.dateofBirthInput);
         firstName = (EditText) findViewById(R.id.firstnameInput);
         lastName = (EditText) findViewById(R.id.lastNameInput);
+
+        userString = (TextView) findViewById(R.id.textView2);
+        phoneNumString = (TextView) findViewById(R.id.textView6);
+        emailString = (TextView) findViewById(R.id.textView);
+
 
         Button createNewUser = (Button) findViewById(R.id.createNewUser);
 
@@ -61,33 +73,67 @@ public class SignUpActivity extends AppCompatActivity implements NoticeDialogFra
             String firstNameText = firstName.getText().toString();
             String lastNameText = lastName.getText().toString();
 
+            if (TextUtils.isEmpty(phoneNumText.trim())) {
+                phoneNumString.setTextColor(Color.RED);
+            }
+            else {
+                phoneNumString.setTextColor(Color.LTGRAY);
+            }
+
+            if (TextUtils.isEmpty(usernameText.trim())){
+                userString.setTextColor(Color.RED);
+            }
+            else {
+                userString.setTextColor(Color.LTGRAY);
+            }
+
+            if (TextUtils.isEmpty(emailText.trim())){
+                emailString.setTextColor(Color.RED);
+            }
+            else {
+                emailString.setTextColor(Color.LTGRAY);
+            }
+
             try
             {
-                ElasticSearchUser.getObjects getter = new ElasticSearchUser.getObjects();
-                getter.execute(usernameText);
-                ArrayList<User> users = getter.get();
-
-                if (users.size()==0)
-                {
-                    User user = new User(usernameText, firstNameText, lastNameText, dateOfBirthText, phoneNumText, emailText);
-
-                    UserController.setContext(SignUpActivity.this);
-                    UserController.saveUser(user);
-
-                    AddUserCommand addUser = new AddUserCommand(user);
-                    MacroCommand.addCommand(addUser);
-                    finish();
-                }
-                else
-                {
+                if(TextUtils.isEmpty(phoneNumText.trim()) || TextUtils.isEmpty(usernameText.trim()) || TextUtils.isEmpty(emailText.trim())){
                     Bundle bundle = new Bundle();
-                    bundle.putString(getResources().getString(R.string.message), getResources().getString(R.string.usernameExitsMessage));
-                    bundle.putString(getResources().getString(R.string.positiveInput), getResources().getString(R.string.ok));
-                    bundle.putString(getResources().getString(R.string.negativeInput), getResources().getString(R.string.login));
+                    bundle.putString("message", getResources().getString(R.string.validateFieldsMessage));
+                    bundle.putString(getResources().getString(R.string.positiveInput), "OK");
 
                     DialogFragment dialog = new NoticeDialogFragment();
                     dialog.setArguments(bundle);
                     dialog.show(getSupportFragmentManager(), "NoticeDialogFragment");
+                    return;
+                }
+                else{
+                    ElasticSearchUser.getObjects getter = new ElasticSearchUser.getObjects();
+                    getter.execute(usernameText);
+                    ArrayList<User> users = getter.get();
+
+                    if (users.size()==0)
+                    {
+
+                        User user = new User(usernameText, firstNameText, lastNameText, dateOfBirthText, phoneNumText, emailText);
+
+                        UserController.setContext(SignUpActivity.this);
+                        UserController.saveUser(user);
+
+                        AddUserCommand addUser = new AddUserCommand(user);
+                        MacroCommand.addCommand(addUser);
+                        finish();
+                    }
+                    else
+                    {
+                        Bundle bundle = new Bundle();
+                        bundle.putString(getResources().getString(R.string.message), getResources().getString(R.string.usernameExitsMessage));
+                        bundle.putString(getResources().getString(R.string.positiveInput), getResources().getString(R.string.ok));
+                        bundle.putString(getResources().getString(R.string.negativeInput), getResources().getString(R.string.login));
+
+                        DialogFragment dialog = new NoticeDialogFragment();
+                        dialog.setArguments(bundle);
+                        dialog.show(getSupportFragmentManager(), "NoticeDialogFragment");
+                    }
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -114,5 +160,9 @@ public class SignUpActivity extends AppCompatActivity implements NoticeDialogFra
     @Override
     public void onDialogNegativeClick(DialogFragment dialog) {
         finish();
+    }
+
+    public void changeEmptyFieldsText(){
+
     }
 }
