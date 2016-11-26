26898c10dece054002728bc7ef5e8076cfe44844
Jessica Huynh <jhuynh@ualberta.ca>
2016-11-25 17:36:40 -0700

    pls

--- app/src/main/java/com/youber/cmput301f16t15/youber/commands/AddRequestCommand.java
+++ app/src/main/java/com/youber/cmput301f16t15/youber/commands/AddRequestCommand.java
@@ -3,6 +3,7 @@ package com.youber.cmput301f16t15.youber.commands;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import com.youber.cmput301f16t15.youber.elasticsearch.ElasticSearchController;
 import com.youber.cmput301f16t15.youber.elasticsearch.ElasticSearchRequest;
 import com.youber.cmput301f16t15.youber.requests.Request;
 
@@ -18,9 +19,21 @@ public class AddRequestCommand extends RequestCommand {
 
     @Override
     public void execute() {
-        ElasticSearchRequest.add adder = new ElasticSearchRequest.add();
-        adder.execute(request);
-        executionState = true;
+        try {
+            // This is for the specific case where we were offline and add an offer as a driver
+            // but this request had already gotten a selected driver (someone else)
+            Request esRequest = ElasticSearchController.getRequest(request.getUUID());
+            if(!esRequest.getDriverUsernameID().isEmpty() && request.getDriverUsernameID().isEmpty()) {
+                executionState = true;
+                return;
+            }
+
+            ElasticSearchRequest.add adder = new ElasticSearchRequest.add();
+            adder.execute(request);
+            executionState = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
